33768aa6f81345dddec6c38baf876c753cf0201e
Troy Davisson <troy.davisson@gmail.com>
2014-06-21 18:09:04 -0400

    Fixed how the integration tests are organized

--- tests/Integration/GetMetadataIntegrationTest.php
+++ tests/Integration/GetMetadataIntegrationTest.php
@@ -1,28 +1,7 @@
 <?php
 
-class GetMetadataIntegrationTest extends PHPUnit_Framework_TestCase
+class GetMetadataIntegrationTest extends BaseIntegration
 {
-    protected $client;
-    /** @var \PHRETS\Session */
-    protected $session;
-
-    public function setUp()
-    {
-        $client = new GuzzleHttp\Client;
-        $watcher = new Gsaulmon\GuzzleRecorder\GuzzleRecorder(__DIR__ . '/Fixtures/Http');
-        $client->getEmitter()->attach($watcher);
-        \PHRETS\Http\Client::set($client);
-
-        $config = new \PHRETS\Configuration;
-        $config->setLoginUrl('http://retsgw.flexmls.com/rets2_1/Login')
-                ->setUsername(getenv('PHRETS_TESTING_USERNAME'))
-                ->setPassword(getenv('PHRETS_TESTING_PASSWORD'))
-                ->setRetsVersion('1.7.2');
-
-        $this->session = new PHRETS\Session($config);
-        $this->session->Login();
-    }
-
     /**
      * System
      */
--- tests/Integration/SessionIntegrationTest.php
+++ tests/Integration/SessionIntegrationTest.php
@@ -1,28 +1,7 @@
 <?php
 
-class SessionIntegrationTest extends PHPUnit_Framework_TestCase
+class SessionIntegrationTest extends BaseIntegration
 {
-    protected $client;
-    /** @var \PHRETS\Session */
-    protected $session;
-
-    public function setUp()
-    {
-        $client = new GuzzleHttp\Client;
-        $watcher = new Gsaulmon\GuzzleRecorder\GuzzleRecorder(__DIR__ . '/Fixtures/Http');
-        $client->getEmitter()->attach($watcher);
-        \PHRETS\Http\Client::set($client);
-
-        $config = new \PHRETS\Configuration;
-        $config->setLoginUrl('http://retsgw.flexmls.com/rets2_1/Login')
-                ->setUsername(getenv('PHRETS_TESTING_USERNAME'))
-                ->setPassword(getenv('PHRETS_TESTING_PASSWORD'))
-                ->setRetsVersion('1.7.2');
-
-        $this->session = new PHRETS\Session($config);
-        $this->session->Login();
-    }
-
     /** @test * */
     public function it_logs_in()
     {
