a3bd6652bb75f466448bec385d53eb5f082b617e
Troy Davisson <troy.davisson@gmail.com>
2014-07-11 09:40:17 -0400

    Made the search parser much easier to extend and customize

--- src/Parsers/Search/OneX.php
+++ src/Parsers/Search/OneX.php
@@ -16,54 +16,22 @@ class OneX
             ->setResource($parameters['SearchType'])
             ->setClass($parameters['Class']);
 
-        if (array_key_exists('RestrictedIndicator', $parameters)) {
-            $rs->setRestrictedIndicator($parameters['RestrictedIndicator']);
+        if ($this->getRestrictedIndicator($rets, $xml, $parameters)) {
+            $rs->setRestrictedIndicator($this->getRestrictedIndicator($rets, $xml, $parameters));
         }
 
-        if (isset($xml->DELIMITER)) {
-            // delimiter found so we have at least a COLUMNS row to parse
-            $delimiter_character = chr("{$xml->DELIMITER->attributes()->value}");
-        } else {
-            // assume tab delimited since it wasn't given
-            $delimiter_character = chr("09");
-            $rets->debug('Assuming TAB delimiter since none specified in response');
-        }
-
-        // break out and track the column names in the response
-        $column_names = "{$xml->COLUMNS[0]}";
-        $column_names = preg_replace("/^{$delimiter_character}/", "", $column_names);
-        $column_names = preg_replace("/{$delimiter_character}\$/", "", $column_names);
-        $rs->setHeaders(explode($delimiter_character, $column_names));
+        $rs->setHeaders($this->getColumnNames($rets, $xml, $parameters));
         $rets->debug(count($rs->getHeaders()) . ' column headers/fields given');
 
-        if (isset($xml->DATA)) {
-            foreach ($xml->DATA as $field_data) {
-                $r = new Record;
+        $this->parseRecords($rets, $xml, $parameters, $rs);
 
-                $field_data = (string)$field_data;
-
-                // split up DATA row on delimiter found earlier
-                $field_data = preg_replace("/^{$delimiter_character}/", "", $field_data);
-                $field_data = preg_replace("/{$delimiter_character}\$/", "", $field_data);
-                $field_data = explode($delimiter_character, $field_data);
-
-                foreach ($rs->getHeaders() as $key => $name) {
-                    // assign each value to it's name retrieved in the COLUMNS earlier
-                    $r->set($name, $field_data[$key]);
-                }
-
-                $rs->addRecord($r);
-            }
-        }
-
-        if (isset($xml->COUNT)) {
-            // found the record count returned.  save it
-            $rs->setTotalResultsCount((int)"{$xml->COUNT->attributes()->Records}");
+        if ($this->getTotalCount($rets, $xml, $parameters) !== null) {
+            $rs->setTotalResultsCount($this->getTotalCount($rets, $xml, $parameters));
             $rets->debug($rs->getTotalResultsCount() . ' total results found');
         }
         $rets->debug($rs->getReturnedResultsCount() . ' results given');
 
-        if (isset($xml->MAXROWS)) {
+        if ($this->foundMaxRows($rets, $xml, $parameters)) {
             // MAXROWS tag found.  the RETS server withheld records.
             // if the server supports Offset, more requests can be sent to page through results
             // until this tag isn't found anymore.
@@ -71,10 +39,100 @@ class OneX
             $rets->debug('Maximum rows returned in response');
         }
 
-        if (isset($xml)) {
-            unset($xml);
-        }
+        unset($xml);
 
         return $rs;
+    }
+
+    /**
+     * @param Session $rets
+     * @param $xml
+     * @param $parameters
+     * @return string
+     */
+    protected function getDelimiter(Session $rets, $xml, $parameters)
+    {
+        if (isset($xml->DELIMITER)) {
+            // delimiter found so we have at least a COLUMNS row to parse
+            return chr("{$xml->DELIMITER->attributes()->value}");
+        } else {
+            // assume tab delimited since it wasn't given
+            $rets->debug('Assuming TAB delimiter since none specified in response');
+            return chr("09");
+        }
+    }
+
+    /**
+     * @param Session $rets
+     * @param $xml
+     * @param $parameters
+     * @return string|null
+     */
+    protected function getRestrictedIndicator(Session $rets, &$xml, $parameters)
+    {
+        if (array_key_exists('RestrictedIndicator', $parameters)) {
+            return $parameters['RestrictedIndicator'];
+        } else {
+            return null;
+        }
+    }
+
+    protected function getColumnNames(Session $rets, &$xml, $parameters)
+    {
+        $delim = $this->getDelimiter($rets, $xml, $parameters);
+
+        // break out and track the column names in the response
+        $column_names = "{$xml->COLUMNS[0]}";
+
+        // take out the first delimiter
+        $column_names = preg_replace("/^{$delim}/", "", $column_names);
+
+        // take out the last delimiter
+        $column_names = preg_replace("/{$delim}\$/", "", $column_names);
+
+        // parse and return the rest
+        return explode($delim, $column_names);
+    }
+
+    protected function parseRecords(Session $rets, &$xml, $parameters, Results $rs)
+    {
+        if (isset($xml->DATA)) {
+            foreach ($xml->DATA as $line) {
+                $rs->addRecord($this->parseRecordFromLine($rets, $xml, $parameters, $line, $rs));
+            }
+        }
+    }
+
+    protected function parseRecordFromLine(Session $rets, &$xml, $parameters, &$line, Results $rs)
+    {
+        $delim = $this->getDelimiter($rets, $xml, $parameters);
+
+        $r = new Record;
+        $field_data = (string)$line;
+
+        // split up DATA row on delimiter found earlier
+        $field_data = preg_replace("/^{$delim}/", "", $field_data);
+        $field_data = preg_replace("/{$delim}\$/", "", $field_data);
+        $field_data = explode($delim, $field_data);
+
+        foreach ($rs->getHeaders() as $key => $name) {
+            // assign each value to it's name retrieved in the COLUMNS earlier
+            $r->set($name, $field_data[$key]);
+        }
+        return $r;
+    }
+
+    protected function getTotalCount(Session $rets, &$xml, $parameters)
+    {
+        if (isset($xml->COUNT)) {
+            return (int)"{$xml->COUNT->attributes()->Records}";
+        } else {
+            return null;
+        }
+    }
+
+    protected function foundMaxRows(Session $rets, &$xml, $parameters)
+    {
+        return isset($xml->MAXROWS);
     }
 }
