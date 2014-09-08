0cec4307f1ea0c9bec2b9b6fc395e5a4655abd46
Troy Davisson <troy.davisson@gmail.com>
2014-09-08 01:47:07 -0400

    Tests for HTTP POST option

--- tests/Integration/SessionIntegrationTest.php
+++ tests/Integration/SessionIntegrationTest.php
@@ -59,4 +59,26 @@ class SessionIntegrationTest extends BaseIntegration
         $this->assertInstanceOf('\PHRETS\Models\Bulletin', $bulletin);
         $this->assertRegExp('/found an Action/', $bulletin->getBody());
     }
+
+    /** @test **/
+    public function it_uses_http_post_method_when_desired()
+    {
+        $config = new \PHRETS\Configuration;
+
+        // this endpoint doesn't actually exist, but the response is mocked, so...
+        $config->setLoginUrl('http://retsgw.flexmls.com/rets2_1/Login')
+                ->setUsername(getenv('PHRETS_TESTING_USERNAME'))
+                ->setPassword(getenv('PHRETS_TESTING_PASSWORD'))
+                ->setRetsVersion('1.7.2')
+                ->setOption('use_post_method', true);
+
+        $session = new \PHRETS\Session($config);
+        $session->Login();
+
+        $system = $session->GetSystemMetadata();
+        $this->assertSame('demomls', $system->getSystemId());
+
+        $results = $session->Search('Property', 'A', '*', ['Limit' => 1, 'Select' => 'LIST_1']);
+        $this->assertCount(1, $results);
+    }
 }
