d4e180fe53a26b9c9f4eff95633aeacdbc91e3f3
Troy Davisson <tdavisson@fbsdata.com>
2017-09-29 19:55:49 -0400

    Properly handle optional info-token-type on 1.8 login responses.  Fixes #170

--- src/Parsers/Login/OneEight.php
+++ src/Parsers/Login/OneEight.php
@@ -1,5 +1,7 @@
 <?php namespace PHRETS\Parsers\Login;
 
+use Illuminate\Support\Str;
+
 class OneEight extends OneX
 {
     public function readLine($line)
@@ -13,14 +15,29 @@ class OneEight extends OneX
 
         if ($name == 'Info') {
             if ($value) {
-                list($name, $type, $value) = explode(';', $value);
-                if ($type == 'Int') {
-                    $value = (int) $value;
-                } elseif ($type == 'Boolean') {
-                    $value = (bool) $value;
+                // break it up on the 2 required parts
+                list($info_token_name, $info_token_value) = explode(';', $value, 2);
+
+                $info_token_type = null;
+                
+                // see if the optional 3rd part was given
+                if (Str::contains($info_token_value, ';')) {
+                    // they included the optional type
+                    list($info_token_type, $info_token_value) = explode(';', $info_token_value);
+
+                    if ($info_token_type == 'Int') {
+                        $info_token_value = (int) $info_token_value;
+                    } elseif ($info_token_type == 'Boolean') {
+                        $info_token_value = (bool) $info_token_value;
+                    } else {
+                        $info_token_value = trim($info_token_value);
+                    }
                 } else {
-                    $value = trim($value);
+                    $info_token_value = trim($info_token_value);
                 }
+
+                $name = $info_token_name;
+                $value = $info_token_value;
             }
         }
 
