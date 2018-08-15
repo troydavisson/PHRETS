6811763a2cd940f09a43c7cd0deef8a45388c1c3
droletmarc <mdrolet@reol.com>
2018-08-15 15:46:12 -0400

    XmlParseException on login #73     SimpleXmlElement return an error when the content of the string is not valid UTF-8 chars.  I've make sure the content is UTF-8. other chars get discarded

--- src/Parsers/XML.php
+++ src/Parsers/XML.php
@@ -11,6 +11,20 @@ class XML
             $string = $string->getBody()->__toString();
         }
 
-        return new \SimpleXMLElement((string) $string);
+        $string = (string) $string;
+
+        /**
+         * Some rets provider(s) return invalid XML data.
+         * Here we make sure the returned data are UTF-8 valid chars.
+         * I've seen so far:
+         *  - windows carriage return (^M at the end of each lines)
+         *  - trade mark sign not converted to &trade; and I guess we can have other sigh like this.
+         *
+         * NOTE:
+         *  //IGNORE will discard the invalid UTF-8 chars.
+         */
+        $string = iconv(mb_detect_encoding($string), 'UTF-8//IGNORE', $string);
+
+        return new \SimpleXMLElement($string);
     }
 }
