845a244f8a7d9b270845ed1223422a0befb8e221
Brandon Surowiec <BrandonSurowiec@users.noreply.github.com>
2018-12-18 17:43:45 -0500

    Fix issue #172 with tempnam() throwing an Exception.
    I have a Homestead server setup on my Windows PC for development and so my temp directory is not "/tmp" and Laravel throws an ErrorException. The `sys_get_temp_dir()` function returns the default path PHP stores temporary files in. So it will still resolve to `"/tmp"` on Linux, but for Windows it will resolve to the correct temp directory.

--- src/Session.php
+++ src/Session.php
@@ -543,7 +543,7 @@ class Session
                 'Accept-Encoding' => 'gzip',
                 'Accept' => '*/*',
             ],
-            'curl' => [ CURLOPT_COOKIEFILE => tempnam('/tmp', 'phrets') ]
+            'curl' => [ CURLOPT_COOKIEFILE => tempnam(sys_get_temp_dir(), 'phrets') ]
         ];
 
         // disable following 'Location' header (redirects) automatically
