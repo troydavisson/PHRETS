211172a4c9d1cea8b8c06cf5e4b5947b99134e94
Troy Davisson <troy.davisson@gmail.com>
2019-04-11 23:45:13 -0400

    Hold cookies in memory for the length of the session.  No more /tmp files

--- src/Session.php
+++ src/Session.php
@@ -543,7 +543,7 @@ class Session
                 'Accept-Encoding' => 'gzip',
                 'Accept' => '*/*',
             ],
-            'curl' => [ CURLOPT_COOKIEFILE => tempnam('/tmp', 'phrets') ]
+            'curl' => [ CURLOPT_COOKIEFILE => '' ]
         ];
 
         // disable following 'Location' header (redirects) automatically
